358a9cba33556850b2f42eab64c3df5297d94102
youdeee <youdee.t.ud@gmail.com>
2015-09-08 23:28:03 +0900

    ediff helm-ls-git->C-c C-f

--- inits/88_other.el
+++ inits/88_other.el
@@ -1,3 +1,8 @@
+;; コントロール用のバッファを同一フレーム内に表示
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; diffのバッファを上下ではなく左右に並べる
+(setq ediff-split-window-function 'split-window-horizontally)
+
 (smart-newline-mode 1)
 
 (setq magit-last-seen-setup-instructions "1.4.0")
