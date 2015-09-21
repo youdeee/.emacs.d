eb067040333c8f87c00c7503a4f2d1a225e42aed
youdeee <youdee.t.ud@gmail.com>
2015-09-21 20:07:45 +0900

    yasni:C-q split3:M-z

--- inits/50_yasnippet.el
+++ inits/50_yasnippet.el
@@ -2,7 +2,7 @@
 
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
-(define-key yas-minor-mode-map (kbd "M-z") 'yas-expand-from-trigger-key)
+(define-key yas-minor-mode-map (kbd "C-q") 'yas-expand-from-trigger-key)
 
 (defun my-yas/prompt (prompt choices &optional display-fn)
   (let* ((names (loop for choice in choices
