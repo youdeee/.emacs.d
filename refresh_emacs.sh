8eda1c223f1fb17ba87a4c543a6a88be408622a6
youdeee <youdee.t.ud@gmail.com>
2015-10-29 01:33:56 +0900

    refresh修正

--- refresh_emacs.sh
+++ refresh_emacs.sh
@@ -3,7 +3,7 @@
 brew install editorconfig
 brew install cmigemo --HEAD
 
-mv inits/el-get/tomorrow-theme/GNU\Emacs/* inits/el-get/tomorrow-theme/
+mv inits/el-get/tomorrow-theme/GNU\ Emacs/* inits/el-get/tomorrow-theme/
 sudo rm -r inits/el-get/tomorrow-theme/*/
 rm inits/el-get/tomorrow-theme/.gitignore
 rm inits/el-get/tomorrow-theme/*.md
