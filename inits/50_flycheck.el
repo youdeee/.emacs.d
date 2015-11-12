893f91b777e63b5ace6554957550212ab459c910
Yudai Takahashi <takahashi@userlocal.jp>
2015-11-12 10:56:51 +0900

    flycheck on

--- inits/50_flycheck.el
+++ inits/50_flycheck.el
@@ -1,1 +1,4 @@
-;;(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+;; (eval-after-load 'flycheck
+;;   '(custom-set-variables
+;;    '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
