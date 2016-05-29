4f9f29843b732d845a27b2ab7133162cd8f22dcb
youdeee <youdee.t.ud@gmail.com>
2016-05-30 01:42:40 +0900

    keychordとりあえず無効化

--- inits/80_language.el
+++ inits/80_language.el
@@ -137,7 +137,23 @@
 (require 'slime)
 (slime-setup '(slime-repl slime-fancy slime-banner))
 (require 'ac-slime)
-(add-hook 'slime-mode-hook 'set-up-slime-ac)
+;;(add-hook 'slime-mode-hook 'set-up-slime-ac)
 ;;(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
-
 ;;(add-hook emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+
+;; Apropos
+(push '("*slime-apropos*") popwin:special-display-config)
+;; Macroexpand
+(push '("*slime-macroexpansion*") popwin:special-display-config)
+;; Help
+(push '("*slime-description*") popwin:special-display-config)
+;; Compilation
+(push '("*slime-compilation*" :noselect t) popwin:special-display-config)
+;; Cross-reference
+(push '("*slime-xref*") popwin:special-display-config)
+;; Debugger
+(push '(sldb-mode :stick t) popwin:special-display-config)
+;; REPL
+(push '(slime-repl-mode) popwin:special-display-config)
+;; Connections
+(push '(slime-connection-list-mode) popwin:special-display-config)
