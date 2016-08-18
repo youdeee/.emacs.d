26512e00e59a9a4612da708ec99c2f74382bdfd2
Yudai Takahashi <takahashi@userlocal.jp>
2016-08-18 16:19:16 +0900

    recentf修正

--- inits/40_recentf.el
+++ inits/40_recentf.el
@@ -4,8 +4,8 @@
 (setq recentf-max-menu-items 500)
 (setq recentf-max-saved-items 3000)
 (custom-set-variables '(recentf-save-file "~/.emacs.d/.recentf"))
-(setq recentf-exclude '(".recentf"))           ;; .recentf自体は含まない
+(setq recentf-exclude '(".recentf"))
 (setq recentf-auto-cleanup 'never)
-(run-with-idle-timer 30 t '(lambda ()          ;; 30秒ごとに .recentf を保存
-   (with-suppressed-message (recentf-save-list))))
+(setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
+
 (require 'recentf-ext)
