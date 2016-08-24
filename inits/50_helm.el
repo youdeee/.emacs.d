aad6f1821d697fcf0188f51bda03b25b21e922df
youdeee <youdee.t.ud@gmail.com>
2016-08-24 11:41:53 +0900

    helm修正

--- inits/50_helm.el
+++ inits/50_helm.el
@@ -1,4 +1,3 @@
-(require 'helm)
 (require 'helm-config)
 (require 'helm-ag)
 (require 'migemo)
@@ -17,7 +16,7 @@
 (setq helm-lisp-fuzzy-completion t)
 (setq helm-mode-fuzzy-match t)
 (setq helm-completion-in-region-fuzzy-match t)
-(setq helm-candidate-number-limit 20)
+;;(setq helm-candidate-number-limit 1000)
 
 ;;(setq helm-autoresize-mode 1)
 ;;(setq helm-split-window-default-side 'right)
@@ -49,34 +48,34 @@
 ;;                    (volatile) (match identity)))
 ;;        source)))
 
-;; フィルタリング2のロジックをいい感じ
-(defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
-  "Transform the pattern to reflect my intention"
-  (let* ((pattern (ad-get-arg 0))
-         (input-pattern (file-name-nondirectory pattern))
-         (dirname (file-name-directory pattern)))
-    (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
-    (setq ad-return-value
-          (concat dirname
-                  (if (string-match "^\\^" input-pattern)
-                      ;; '^' is a pattern for basename
-                      ;; and not required because the directory name is prepended
-                      (substring input-pattern 1)
-                    (concat ".*" input-pattern))))))
-(defun helm-buffers-list-pattern-transformer (pattern)
-  (if (equal pattern "")
-      pattern
-    ;; Escape '.' to match '.' instead of an arbitrary character
-    (setq pattern (replace-regexp-in-string "\\." "\\\\." pattern))
-    (let ((first-char (substring pattern 0 1)))
-      (cond ((equal first-char "*")
-             (concat " " pattern))
-            ((equal first-char "=")
-             (concat "*" (substring pattern 1)))
-            (t
-             pattern)))))
-(add-to-list 'helm-source-buffers-list
-             '(pattern-transformer helm-buffers-list-pattern-transformer))
+;; ;; フィルタリング2のロジックをいい感じ
+;; (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+;;   "Transform the pattern to reflect my intention"
+;;   (let* ((pattern (ad-get-arg 0))
+;;          (input-pattern (file-name-nondirectory pattern))
+;;          (dirname (file-name-directory pattern)))
+;;     (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+;;     (setq ad-return-value
+;;           (concat dirname
+;;                   (if (string-match "^\\^" input-pattern)
+;;                       ;; '^' is a pattern for basename
+;;                       ;; and not required because the directory name is prepended
+;;                       (substring input-pattern 1)
+;;                     (concat ".*" input-pattern))))))
+;; (defun helm-buffers-list-pattern-transformer (pattern)
+;;   (if (equal pattern "")
+;;       pattern
+;;     ;; Escape '.' to match '.' instead of an arbitrary character
+;;     (setq pattern (replace-regexp-in-string "\\." "\\\\." pattern))
+;;     (let ((first-char (substring pattern 0 1)))
+;;       (cond ((equal first-char "*")
+;;              (concat " " pattern))
+;;             ((equal first-char "=")
+;;              (concat "*" (substring pattern 1)))
+;;             (t
+;;              pattern)))))
+;; (add-to-list 'helm-source-buffers-list
+;;              '(pattern-transformer helm-buffers-list-pattern-transformer))
 
 ;; (require 'ag)
 ;; (setq default-process-coding-system 'utf-8-unix)  ; ag 検索結果のエンコード指定
