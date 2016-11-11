24ed00d035c72daf8a65fa62cef6062249b0f2f7
youdeee <youdee.t.ud@gmail.com>
2016-11-12 02:44:42 +0900

    elscreen persist

--- inits/88_other.el
+++ inits/88_other.el
@@ -114,6 +114,7 @@
         ("dict" . "OnlineDict")
         ("*WL:Message*" . "Wanderlust")))
 (elscreen-separate-buffer-list-mode 1)
+(elscreen-persist-mode 1)
 
 (tabbar-mode 1)
 (setq tabbar-use-images nil)
